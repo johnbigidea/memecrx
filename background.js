18e9420523b9ef894def291e5f11d87e357c1099
z <rzha321@gmail.com>
2020-01-09 09:32:54 +0800

    v0003 adding booklist and pianolist tab

--- background.js
+++ background.js
@@ -2,18 +2,50 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 'use strict';
+chrome.runtime.onMessage.addListener('hh',function(){
+  alert(hh)
+});
 
 chrome.alarms.onAlarm.addListener(function() {
-  chrome.storage.sync.get(['minutes'], function(item) {
-
-    chrome.notifications.create({
+  chrome.storage.sync.get(['tim','ci'], function(item) {
+    var b = item.ci.toString();
+    var params={
       type:     'basic',
-      iconUrl:  'stay_hydrated.png',
-      title:    'Time to Hydrate',
-      message:  'Everyday I\'m Guzzlin\'!' ,
-      buttons: [
-        {title: 'Keep it Flowing.'}
-      ],
-      priority: 0});
+      iconUrl:  'iconoficon.jpg',
+      title:    '提醒一下，兄弟',
+      message:  b,
+      priority: 0,
+      requireInteraction:true
+    };
+
+  
+    chrome.notifications.create(params);
   });
 });
+
+
+
+// // Copyright 2017 The Chromium Authors. All rights reserved.
+// // Use of this source code is governed by a BSD-style license that can be
+// // found in the LICENSE file.
+// 'use strict';
+
+// chrome.alarms.onAlarm.addListener(function() {
+//   chrome.storage.sync.get(['tim'], function(item) {
+
+//     var a = new String(item.tim)
+   
+
+
+//     var params={
+//       type:     'basic',
+//       iconUrl:  'stay_hydrated.png',
+//       title: 'asdf',
+//       message:  'Everyday I\'m Guzzlin\'!' ,
+//       priority: 0
+//     };
+
+      
+//     chrome.notifications.create(params);
+//   });
+// });
--- popup.js
+++ popup.js
@@ -1,15 +1,26 @@
 'use strict';
 
 let btn = document.getElementById("btn");
+
 let cla = document.getElementById("cla");
 let hista = document.getElementById("hista");
 let nex = document.getElementById("nex");
 let welcome = document.getElementById("welcome");
+let sa = document.getElementById("sa");
 nex.addEventListener("click", reshow);
 var sortlist = new Array()
 var hist = new Array()
 var w = $('ol')
 let clearAm = document.getElementById("al");
+
+var sortlist = new Array()
+var hist = new Array()
+var blist = new Array()
+var plist = new Array()
+
+let dream = document.getElementById("dream");
+let bl = document.getElementById("bl");
+
 
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
@@ -19,6 +30,19 @@ let clearAm = document.getElementById("al");
 // chrome.alarms.create('c',{when: new Date(Date.now()+120*1000).getTime()});
 // chrome.alarms.create('b',{when: p+8*1000});
 clearAm.addEventListener("click", clearall);
+sa.addEventListener("click", reshowall);
+bl.addEventListener("click", showbook);
+
+btn.addEventListener("click", store);
+cla.addEventListener("click", clear);
+hista.addEventListener("click", showHist);
+
+function showbook(){
+
+
+
+
+}
 
 function clearall(){
   chrome.alarms.clearAll();
@@ -26,7 +50,7 @@ function clearall(){
 }
 
 function ralarm(e){
-
+chrome.storage.sync.set({ci:e});
 
 e.sort(compareSecondColumn);
 
@@ -40,9 +64,12 @@ function compareSecondColumn(a, b) {
 }
   chrome.alarms.clearAll();
   for(var i = 0;i<e.length;i++){
-    var a = 'this is ' + i
+
+    var a = 'a' + i
     chrome.alarms.create(a,{when: e[i][2]});
-  } 
+
+  }
+  chrome.runtime.sendMessage({hh:e.length}) 
   
 }
 
@@ -55,7 +82,24 @@ function compareSecondColumn(a, b) {
 // document.getElementById('15min').addEventListener('click', setAlarm);
 // document.getElementById('30min').addEventListener('click', setAlarm);
 // document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
+function reshowall(){
+  w.empty();
+  if(sortlist){
 
+
+  for(var i  = 0;i<sortlist.length;i++) {
+   
+      w.append('<li value = i>' +sortlist[i][0]+'   ------   '+sortlist[i][1]+'<button class="dele" value=' +i+'>delete</button></li>')
+      w.append('<br>')
+      dream.value = "";
+    
+
+      
+    }
+
+}
+
+} 
 
 
 function reshow(){
@@ -64,18 +108,21 @@ function reshow(){
 
 
   for(var i  = 0;i<sortlist.length;i++) {
-    w.append('<li value = i>' +sortlist[i][0]+'   ------   '+sortlist[i][1]+'<button class="dele" value=' +i+'>delete</button></li>')
-    w.append('<br>')
-    name.value = "";
-  }
+    if(new Date(sortlist[i][2]).getDate() == new Date().getDate()){
+      w.append('<li value = i>' +sortlist[i][0]+'   ------   '+sortlist[i][1]+'<button class="dele" value=' +i+'>delete</button></li>')
+      w.append('<br>')
+      dream.value = "";
+    }
+
+      
+    }
+
 }
 
 } 
 
 
 
-var sortlist = new Array()
-var hist = new Array()
 w.on('click','.dele',function(e){
   // $(this).parent().remove();
    var ahashuchuan = e.target.value
@@ -84,13 +131,7 @@ w.on('click','.dele',function(e){
    sortlist.splice(ahashuchuan,1)
 
    chrome.storage.local.set({ 'sortlist': (sortlist)}, ralarm(sortlist));
-
-  w.empty()
-  for(var i  = 0;i<sortlist.length;i++) {
-    w.append('<li value = i>' +sortlist[i][0]+'   ------   '+sortlist[i][1]+'<button class="dele" value=' +i+'>delete</button></li>')
-    w.append('<br>')
-    name.value = "";
-  }
+   reshow();
 
 
   //  updateShow(sortlist);
@@ -134,19 +175,26 @@ w.on('click','.delehis',function(e){
   
 })
 
-let name = document.getElementById("nameField");
 
-btn.addEventListener("click", storeName);
-cla.addEventListener("click", clear);
-// console.log(cla);
-// console.log(hista);
-hista.addEventListener("click", showHist);
+
+
 
 
 function showHist(){
   w.empty();
   for(var i  = 0;i<hist.length;i++) {
     w.append('<li value = i>' +hist[i][0]+ '   ------   '+hist[i][1]+'<button class="delehis" value=' +i+'>delete</button></li>')
+    w.append('<br>')
+
+
+  }
+
+}
+
+function showBlist(){
+  w.empty();
+  for(var i  = 0;i<booklist.length;i++) {
+    w.append('<li value = i>' +booklist[i][0]+ '   ------   '+booklist[i][1]+'<button class="delehis" value=' +i+'>delete</button></li>')
     w.append('<br>')
 
 
@@ -166,6 +214,7 @@ function clear(){
   if(confirm('are you sure')){
     if(confirm('are you really sure')){
       chrome.storage.local.clear(function(obj){
+        clearall();
         console.log("cleared");
         window.location.reload();
       });
@@ -188,6 +237,16 @@ load();
 function load(){
   chrome.storage.local.get('hist', function(result){
     hist = result.hist;
+
+
+  });
+  chrome.storage.local.get('booklist', function(result){
+    booklist = result.booklist;
+
+
+  });
+  chrome.storage.local.get('pianolist', function(result){
+    pianolist = result.pianolist;
 
 
   });
@@ -247,12 +306,15 @@ function load(){
 //   console.log(hist)
 // });
 
-var ebin = [1,5,30,120,360,720,1440,2880,5760,11660]
+var ebin = [0.1,30,120,360,720,1440,2880,5760,11660,28888]
 
-function storeName() {
+function store() {
   var lhist;
-  if (name.value.trim() !== "") {
-    var l;
+  var l;
+  var lblist;
+  var lplist;
+  if (dream.value.trim() !== "") {
+    
     if(typeof sortlist === "undefined"){
       l = 0
       sortlist = new Array(0)
@@ -268,51 +330,48 @@ function storeName() {
   
 
 
-
-
     for(var i  = 0;i<10;i++) {
       // w.append('<li>' + name.value.trim() + '</li>')
       // w.append('<br>')
       var temp = new Array(3)
       let d = new Date()
       sortlist[l+i] = temp
-      sortlist[l+i][0] = name.value
+      sortlist[l+i][0] = dream.value
       sortlist[l+i][1] = new Date(d.getTime()+ebin[i]*60*1000).toLocaleString();
       sortlist[l+i][2] = d.getTime()+ebin[i]*60*1000
 
     }
-
-        
+ 
      var temp2 = new Array(2)
      hist[lhist] = temp2
-     hist[lhist][0] = name.value
+     hist[lhist][0] = dream.value
      hist[lhist][1] = new Date().toLocaleString();
 
-
-
-
-  
-
-    chrome.storage.local.set({ 'hist': (hist)}, function() {
-
-
-
-    });
+    chrome.storage.local.set({ 'hist': (hist)}, function() {});
 
     chrome.storage.local.set({ 'sortlist': (sortlist)}, ralarm(sortlist));
 
     welcome.innerHTML = `NEW, ${sortlist[l][0]}`;
-    w.empty();
+    reshow();
 
-    for(var i  = 0;i<sortlist.length;i++) {
-      w.append('<li value = i>' +sortlist[i][0]+  '   ------   '+sortlist[i][1]+'<button class="dele" value=' +i+'>delete</button></li>')
-      w.append('<br>')
-      name.value = "";
+
+
+  } 
+  if (bl.value.trim() !== "") {
+    if(typeof booklist === "undefined"){
+      lblsit = 0
+      booklist = new Array(0)
+    }else{
+      lblist  = booklist.length;
     }
+    var tempbook = new Array(2)
+    booklist[lblist] = tempbook
+    booklist[lblist][0] = bl.value
+    booklist[lblist][1] = new Date().toLocaleString();
 
-  } else {
-    welcome.innerHTML = "Welcome";
-    name.value = "";
+    chrome.storage.local.set({ 'booklist': (booklist)}, function() {});
+    showBlist();
+
   }
 }
 
